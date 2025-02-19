1cc27e08f0566d6894d5f6ab030232de63e84795
sidsrirampsu <siddhikasriram.s@gmail.com>
2025-02-19 13:34:02 -0500

    Add Basics Java Programs

---
+++ basics/VariableDeclaration.java
@@ -0,0 +1,13 @@
+public class VariableDeclaration{  
+    public static void main(String[] args) {
+        int num = 10;
+        double decimal = 20.5;
+        char letter = 'A';
+        boolean flag = true;
+
+        System.out.println("Integer: " + num);
+        System.out.println("Double: " + decimal);
+        System.out.println("Character: " + letter);
+        System.out.println("Boolean: " + flag);
+    }
+}
